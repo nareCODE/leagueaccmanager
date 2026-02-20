9492b63a93dac1cad49daff7a1316e54eb706249
nare <maxime@le-mole.com>
2026-02-20 19:23:50 +0100

    run.bat

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 @echo off
 :: Check if npm is installed
-npm -v >nul 2>&1
+call npm -v >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo npm is not installed. Installing Node.js now...
     winget install OpenJS.NodeJS -e --silent
     echo Installation finished. Please close this window, open a new one, and run this file again.
     pause
-    exit
+    exit /b
 )
 
 echo Installing dependencies...
